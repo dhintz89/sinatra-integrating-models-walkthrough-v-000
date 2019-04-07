28ca8c67380db809e3a6f6605440742215739c15
Dan Hintz <dhintz@live.com>
2019-04-07 21:18:19 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,2 +1,26 @@
 # Your TextAnalyzer model code will go here.
-class TextAnalyzer < 
+class TextAnalyzer
+  attr_reader :text
+  
+  def initialize(text)
+    @text = text.downcase
+  end
+  
+  def count_of_words
+    words = text.split(" ")
+    words.count
+  end
+  
+  def count_of_vowels
+    text.scan(/[aeoui]/).count
+  end
+  
+  def count_of_consonants
+    text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+  end
+  
+  def most_used_letter
+    s1 = text.gsub(/[^a-z]/, '')
+    arr = s1.split('')
+    arr1 = arr.uniq
+    arr2 = {}
